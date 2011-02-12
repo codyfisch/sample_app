5784db78c3c291867632e36770c58175e2c48bff
Cody Fischel <cody.fischel@gmail.com>
2011-02-12 13:56:53 -0500

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,14 @@ SampleApp::Application.routes.draw do
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
 
+  get "users/new"
+
+  match '/signup',  :to => 'users#new'
+  
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+
   root :to => 'pages#home'
 
 
